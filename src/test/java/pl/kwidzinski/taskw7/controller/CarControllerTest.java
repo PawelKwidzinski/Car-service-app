744a836cf3aa3f23e7397cefcc123e5d0fe92b5b
PawelKwidzinski <solvap83@gmail.com>
2021-03-16 16:25:26 +0100

    Added test: should_return_list_of_cars()

--- src/test/java/pl/kwidzinski/taskw7/controller/CarControllerTest.java
+++ src/test/java/pl/kwidzinski/taskw7/controller/CarControllerTest.java
@@ -36,7 +36,33 @@ class CarControllerTest {
 
     @Test
     void should_return_list_of_cars() throws Exception {
+//given
+        List<Car> cars = List.of(
+                new Car(1L, "VW", "New Beatle", LocalDate.of(2016, 12, 15), Color.WHITE),
+                new Car(2L, "VW", "Golf III", LocalDate.of(1996, 2, 18), Color.BLACK),
+                new Car(3L, "Toyota", "Yaris", LocalDate.of(2018, 3, 19), Color.BLUE),
+                new Car(4L, "Fiat", "Panda", LocalDate.of(2008, 8, 9), Color.BLUE));
 
+        //when
+        when(carDao.findAll()).thenReturn(cars);
+        mockMvc.perform(MockMvcRequestBuilders.get("/cars/list"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(view().name("car-main"))
+                .andExpect(model().attributeExists("allCars"))
+                .andExpect(model().attribute("allCars", Matchers.hasSize(4)))
+                .andExpect(model().attribute("allCars", Matchers.hasItem(Matchers.allOf(
+                        Matchers.<Car>hasProperty("id", Is.is(1L)),
+                        Matchers.<Car>hasProperty("brand", Is.is("VW")),
+                        Matchers.<Car>hasProperty("model", Is.is("New Beatle")),
+                        Matchers.<Car>hasProperty("production", Is.is(LocalDate.of(2016, 12, 15))),
+                        Matchers.<Car>hasProperty("color", Is.is(Color.WHITE))))))
+
+                .andExpect(model().attribute("allCars", Matchers.hasItem(Matchers.allOf(
+                        Matchers.<Car>hasProperty("id", Is.is(4L)),
+                        Matchers.<Car>hasProperty("brand", Is.is("Fiat")),
+                        Matchers.<Car>hasProperty("model", Is.is("Panda")),
+                        Matchers.<Car>hasProperty("production", Is.is(LocalDate.of(2008, 8, 9))),
+                        Matchers.<Car>hasProperty("color", Is.is(Color.BLUE))))));
     }
 
     @Test
